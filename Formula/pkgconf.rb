e82b25df9d862c376bed43840cd8b132356ead83
Graham Ollis <plicease@cpan.org>
2018-09-02 16:54:49 -0400

    update description and homepage

--- Formula/pkgconf.rb
+++ Formula/pkgconf.rb
@@ -2,8 +2,8 @@
 #                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Pkgconf < Formula
-  desc ""
-  homepage ""
+  desc "pkg-config compatible tool and API"
+  homepage "http://pkgconf.org/"
   url "https://distfiles.dereferenced.org/pkgconf/pkgconf-1.5.3.tar.gz"
   sha256 "0c5a750af68bbb06be542d359af57003dc66926533b64b00a3fefc776819f41e"
   # depends_on "cmake" => :build
